0d58d16d5b4fbfbaf0853e375ddb6ffc3774c872
jmihelson <jmihelson@gmail.com>
2023-05-07 16:29:29 +0300

    esimesed sammud

---
+++ src/components/MainPage.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function MainPage() {
+  return (
+    <main>
+      <div className="retsept">
+        <img className="pilt" src="./src/Pic/retsept-1.png" alt="food" />
+        <h3 className="text">Retsepti Nimi</h3>
+        <p className="text">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel rem
+          perferendis laborum fugiat asperiores quo iusto adipisci incidunt
+          maxime sunt aspernatur nulla aut consectetur iure accusantium
+          consequuntur, ullam itaque fugit!
+        </p>
+
+        <img className="heart" src="./src/Pic/heart.png" alt="Heart" />
+
+        <div />
+        
+      </div>
+    </main>
+  );
+}
+
+export default MainPage;
