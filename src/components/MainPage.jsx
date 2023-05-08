c5d9b8479952a10e37f362bfe0a4e59fa24d73ee
jmihelson <jmihelson@gmail.com>
2023-05-08 21:39:15 +0300

    08.05.23

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function MainPage() {
   return (
-    <main>
+    <main className="main-page">
       <div className="retsept">
         <img className="pilt" src="./src/Pic/retsept-1.png" alt="food" />
         <h3 className="text">Retsepti Nimi</h3>
@@ -14,9 +14,39 @@ function MainPage() {
         </p>
 
         <img className="heart" src="./src/Pic/heart.png" alt="Heart" />
+        <p className="likes">123</p>
 
         <div />
-        
+      </div>
+      <div className="retsept">
+        <img className="pilt" src="./src/Pic/retsept-1.png" alt="food" />
+        <h3 className="text">Retsepti Nimi</h3>
+        <p className="text">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel rem
+          perferendis laborum fugiat asperiores quo iusto adipisci incidunt
+          maxime sunt aspernatur nulla aut consectetur iure accusantium
+          consequuntur, ullam itaque fugit!
+        </p>
+
+        <img className="heart" src="./src/Pic/heart.png" alt="Heart" />
+        <p className="likes">123</p>
+
+        <div />
+      </div>
+      <div className="retsept">
+        <img className="pilt" src="./src/Pic/retsept-1.png" alt="food" />
+        <h3 className="text">Retsepti Nimi</h3>
+        <p className="text">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel rem
+          perferendis laborum fugiat asperiores quo iusto adipisci incidunt
+          maxime sunt aspernatur nulla aut consectetur iure accusantium
+          consequuntur, ullam itaque fugit!
+        </p>
+
+        <img className="heart" src="./src/Pic/heart.png" alt="Heart" />
+        <p className="likes">123</p>
+
+        <div />
       </div>
     </main>
   );
