2807577f1aa8a23adf1776665f16864cf3d238a6
jmihelson <jmihelson@gmail.com>
2023-05-19 00:08:59 +0300

    uuendused

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,66 +1,65 @@
 import React from "react";
 
-
 function MainPage() {
   return (
-    
     <>
-      
-        <input className="search" name="search" defaultValue="Search" />
-      
+      <input className="search" name="search" defaultValue="Search" />
 
-      <img className="foodful-logo" src="./src/Pic/foodful-logo.png" alt="foodful" />
+      <img
+        className="foodful-logo"
+        src="./src/Pic/foodful-logo.png"
+        alt="foodful"
+      />
 
       <button className="login">Login</button>
-    
 
-    <div className="retseptid">
-      <div className="retsept-1">
-        <img className="pilt" src="./src/Pic/retsept-1.png" alt="food" />
-        <h3 className="text">Retsepti Nimi</h3>
-        <p className="text">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel rem
-          perferendis laborum fugiat asperiores quo iusto adipisci incidunt
-          maxime sunt aspernatur nulla aut consectetur iure accusantium
-          consequuntur, ullam itaque fugit!
-        </p>
+      <div className="retseptid">
+        <div className="retsept-1">
+          <img className="pilt" src="./src/Pic/retsept-1.png" alt="food" />
+          <h3 className="text">Retsepti Nimi</h3>
+          <p className="text">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel rem
+            perferendis laborum fugiat asperiores quo iusto adipisci incidunt
+            maxime sunt aspernatur nulla aut consectetur iure accusantium
+            consequuntur, ullam itaque fugit!
+          </p>
 
-        <img className="heart" src="./src/Pic/heart.png" alt="Heart" />
-        <p className="likes">123</p>
+          <img className="heart" src="./src/Pic/heart.png" alt="Heart" />
+          <p className="likes">123</p>
 
-        <div />
+          <div />
+        </div>
+        <div className="retsept-2">
+          <img className="pilt" src="./src/Pic/retsept-1.png" alt="food" />
+          <h3 className="text">Retsepti Nimi</h3>
+          <p className="text">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel rem
+            perferendis laborum fugiat asperiores quo iusto adipisci incidunt
+            maxime sunt aspernatur nulla aut consectetur iure accusantium
+            consequuntur, ullam itaque fugit!
+          </p>
+
+          <img className="heart" src="./src/Pic/heart.png" alt="Heart" />
+          <p className="likes">123</p>
+
+          <div />
+        </div>
+        <div className="retsept-3">
+          <img className="pilt" src="./src/Pic/retsept-1.png" alt="food" />
+          <h3 className="text">Retsepti Nimi</h3>
+          <p className="text">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel rem
+            perferendis laborum fugiat asperiores quo iusto adipisci incidunt
+            maxime sunt aspernatur nulla aut consectetur iure accusantium
+            consequuntur, ullam itaque fugit!
+          </p>
+
+          <img className="heart" src="./src/Pic/heart.png" alt="Heart" />
+          <p className="likes">123</p>
+
+          <div />
+        </div>
       </div>
-      <div className="retsept-2">
-        <img className="pilt" src="./src/Pic/retsept-1.png" alt="food" />
-        <h3 className="text">Retsepti Nimi</h3>
-        <p className="text">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel rem
-          perferendis laborum fugiat asperiores quo iusto adipisci incidunt
-          maxime sunt aspernatur nulla aut consectetur iure accusantium
-          consequuntur, ullam itaque fugit!
-        </p>
-
-        <img className="heart" src="./src/Pic/heart.png" alt="Heart" />
-        <p className="likes">123</p>
-
-        <div />
-      </div>
-      <div className="retsept-3">
-        <img className="pilt" src="./src/Pic/retsept-1.png" alt="food" />
-        <h3 className="text">Retsepti Nimi</h3>
-        <p className="text">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel rem
-          perferendis laborum fugiat asperiores quo iusto adipisci incidunt
-          maxime sunt aspernatur nulla aut consectetur iure accusantium
-          consequuntur, ullam itaque fugit!
-        </p>
-
-        <img className="heart" src="./src/Pic/heart.png" alt="Heart" />
-        <p className="likes">123</p>
-
-        <div />
-      </div>
-    </div>
     </>
   );
 }
